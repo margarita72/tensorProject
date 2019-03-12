eb94fe37dd4a065c56343748206fcb8654a6d55c
Gagik <gagikpog@gmail.com>
2019-03-12 22:24:16 +0500

    Feature 3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-/**
- * Функция принимает объект и возвращает строку. 
+/**  
+ * Функция принимает объект и возвращает строку.   
  * Объект передается по рекурсии. 
  * Каждый вызов - это переход на следующий уровень вложения JSON.
  * @param {Object} data - принимает JSON объект.
@@ -98,7 +98,7 @@ function parse(jsonObj)
  * @returns {Object} Объект с полями из ключей и методами delete, read, update.
  * @constructor 
  */
-let modelBuilder = function (keys,values){
+let modelBuilderOld = function (keys,values){
     let res = {};  //Создание пустого объекта.
     if(keys && values)  //Если не передали массив ключей и значений то создаем пустой объект. 
     {
@@ -164,10 +164,99 @@ let modelBuilder = function (keys,values){
 }
 
 //let o = modelBuilder(["name","children"],['title',[['name'],['title1']]]);  
-//let jsonExample  = [{ "name": "Доска 1", "children": [ { "name": "Список задач 1.1", "children": [ { "name": "Задача 1.1.1" }, { "name": "Задача 1.1.2" } ] }, { "name": "Список задач 1.2", "children": [ { "name": "Задача 1.2.1" }, { "name": "Задача 1.2.2" } ] }, { "name": "Список задач 1.3" } ] }, { "name": "Доска 2" } ];  
+let jsonExample  = [{ "name": "Доска 1", "children": [ { "name": "Список задач 1.1", "children": [ { "name": "Задача 1.1.1" }, { "name": "Задача 1.1.2" } ] }, { "name": "Список задач 1.2", "children": [ { "name": "Задача 1.2.1" }, { "name": "Задача 1.2.2" } ] }, { "name": "Список задач 1.3" } ] }, { "name": "Доска 2" } ];  
 //a = [{"name":"Доска 1","children":[{"name": "Список задач 1.1"}]},{"name":"Доска 1"}];
 
-let p1 = ["name","children"];
-let p2 = ["Доска 1",[{"name": "Список задач 1.1"}]];
+//let p1 = ["name","children"];
+//let p2 = ["Доска 1",[{"name": "Список задач 1.1"}]];
 
-parse(modelBuilder(p1,p2).read());
+//parse(modelBuilder(p1,p2).read());
+
+data = [{"id":1,"name":"Доска 1","hasChildren":true},{"id":2,"parent":1,"name":"Список задач 1.1","hasChildren":true},{"id":3,"parent":2,"name":"Задача 1.1.1"},{"id":4,"parent":2,"name":"Задача 1.1.2"},{"id":5,"parent":1,"name":"Список задач 1.2","hasChildren":true},{"id":6,"parent":5,"name":"Задача 1.2.1"},{"id":7,"parent":5,"name":"Задача 1.2.2"},{"id":8,"parent":1,"name":"Список задач 1.3"},{"id":9,"name":"Доска 2"}];
+
+function loadChildren(id, callback){
+    if(callback === undefined || typeof(callback) !== 'function')
+        throw new Error('callback not defined');
+    if(id === null)
+        id = undefined;
+    let res = [];
+    for (let i = 0; i < data.length; i++) {
+        if(data[i].parent == id)
+        {
+           res.push( new modelBuilder(data[i]))
+        }
+    }
+    if (res.length)
+        setTimeout(callback, 0,res);
+}
+class modelBuilder{
+    constructor(obj){
+        this.id = null;
+        this.hasChildren = false;
+        this.name = 'defoult name';
+        this.parent = null;
+        this.update(obj);
+    }
+    delete(){
+        this.removed = true;  //Свойство removed принимает значение true.
+    }
+    update(updObj){
+        for (const i in updObj) {  //Цикл по всем ключам в объекте.
+            if(this[i] !== undefined) //Если поле существует, то изменить значение
+                this[i] = updObj[i];
+        }
+    }
+    read(key){
+        if(this.removed)  //Проверка значения элемента. Если он печень как удаленный, то возвращаем undefined. 
+            return; 
+        if(key) //Если был передан ключ, то функция должна вернуть значение по ключу.
+        {
+            if(this[key]) 
+                return this[key];  //Возвращает значение свойства с ключом key.
+            else return null;  //Если ключа не существует или значение в нём пусто – возвращает null.
+        } 
+        //метод возвращает объект без методов объекта если ключ не передан.
+        let tmp = {};  //Создаётся пустой оъект.
+        let keys = Object.keys(this);
+        for (let i = 0; i < keys.length;i++) { //Перебрать все методы и проя и выделить все поля.
+            if(typeof this[keys[i]] !== 'function') //Если не функйия, то поле со значением копируется в новый объект.
+                tmp[keys[i]] = this[keys[i]]; 
+        }
+        let m = [];
+        m[0] = tmp;
+        return m;  //Возвращаем объект без методов.
+    }
+    getChildren (callback){
+        setTimeout(callback,0,id,cb);
+    }
+    render() {
+        let btn = '';
+        if(this.hasChildren)
+            btn = '<div class="list-open" onclick="OpenLine(this)" id="btn' + this.id + '"></div>';
+        return '<li id="list'+ this.id +'">' + btn + ' ' + this.name + '</li>';
+    }
+}
+function cb(arr)
+{
+    let container = 'main-list'
+    if(arr[0].parent)
+    {
+        container = 'list' +  arr[0].parent;
+    }
+    let li = document.getElementById(container);
+    let res = '';
+    for (let i = 0; i < arr.length; i++) {
+        res += arr[i].render();
+    }
+    li.innerHTML += '<ul>'+res+'</ul>';
+}
+
+function OpenLine(line){
+    console.log('open')
+    if(line.innerText === '0')
+        return ;
+    line.innerText = '0';
+    loadChildren(line.id.substring(3),cb);
+}
+
+loadChildren(null,cb);
