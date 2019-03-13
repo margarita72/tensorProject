f25cc8dae8c594ace770f6d8ef2ae1d3e72c9e5c
Gagik <gagikpog@gmail.com>
2019-03-13 21:52:49 +0500

    its work

--- script.js
+++ script.js
@@ -92,88 +92,14 @@ function parse(jsonObj)
         return;  //Выход из функции.
     }
 }
-/** 
- * @param {Array} keys Массив ключей.
- * @param {Array} values Массив значений.
- * @returns {Object} Объект с полями из ключей и методами delete, read, update.
- * @constructor 
- */
-let modelBuilderOld = function (keys,values){
-    let res = {};  //Создание пустого объекта.
-    if(keys && values)  //Если не передали массив ключей и значений то создаем пустой объект. 
-    {
-        if(keys.length !== values.length)    //Если размеры объектов не эквивалентны, то генерируем исключение.
-            throw new Error('Ключи и значения имеют разные размеры');
-        for (let i = 0; i < keys.length; i++) {  //Цикл обработки элементов массива.
-            res[keys[i]] = values[i];  //Создается новое поле с названием key[i] и с  значением values[i].
-        }
-    }
-    /**
-     * Метод delete, помечает объект для удаления 
-     * (свойство removed принимает значение true).
-     *  @name modelBuilder#delete
-     */
-    res.delete = function(){
-        this.removed = true;  //Свойство removed принимает значение true.
-    }
-    /**
-     * Метод update обновляет объект.
-     * @name modelBuilder#update
-     * @param {Object} updObj Предполагается что во входящем объекте updObj содержаться ключ/значения для обновления.
-     */
-    res.update = function(updObj){
-        for (const i in updObj) {  //Цикл по всем ключам в объекте.
-            if(this[i] !== undefined) //Если поле существует, то изменить значение
-                this[i] = updObj[i];
-        }
-        /*Этот код предназначен для массивов
-        if(!updObj || updObj.length !== 2)  //Если объект updObj не определен или это не пара ключь - значение, то генерируем исключение.
-            throw new Error('Невозможно обновить: переданный объект не совпадает требованиям.');
-        if(this[updObj[0]])
-            this[updObj[0]] = updObj[1];  //Обновляем значение.
-        else throw new Error('У объекта нет такого поля: ' + updObj[0]); //Если у объекта нет такого поля генерируем исключение.
-        */
-    }
-    /**
-     * Метод read возвращает выбранный (key) объект JSON таблицы.
-     * Если ключа не существует или значение в нём пусто – возвращает null.
-     * @name modelBuilder#read
-     * @param {String} key 
-     */
-    res.read = function(key){
-        if(this.removed)  //Проверка значения элемента. Если он печень как удаленный, то возвращаем undefined. 
-            return; 
-        if(key) //Если был передан ключ, то функция должна вернуть значение по ключу.
-        {
-            if(this[key]) 
-                return this[key];  //Возвращает значение свойства с ключом key.
-            else return null;  //Если ключа не существует или значение в нём пусто – возвращает null.
-        } 
-        //метод возвращает объект без методов объекта если ключ не передан.
-        let tmp = {};  //Создаётся пустой оъект.
-        let keys = Object.keys(this);
-        for (let i = 0; i < keys.length;i++) { //Перебрать все методы и проя и выделить все поля.
-            if(typeof this[keys[i]] !== 'function') //Если не функйия, то поле со значением копируется в новый объект.
-                tmp[keys[i]] = this[keys[i]]; 
-        }
-        let m = [];
-        m[0] = tmp;
-        return m;  //Возвращаем объект без методов.
-    }
-    return res;
-}
-
-//let o = modelBuilder(["name","children"],['title',[['name'],['title1']]]);  
-let jsonExample  = [{ "name": "Доска 1", "children": [ { "name": "Список задач 1.1", "children": [ { "name": "Задача 1.1.1" }, { "name": "Задача 1.1.2" } ] }, { "name": "Список задач 1.2", "children": [ { "name": "Задача 1.2.1" }, { "name": "Задача 1.2.2" } ] }, { "name": "Список задач 1.3" } ] }, { "name": "Доска 2" } ];  
-//a = [{"name":"Доска 1","children":[{"name": "Список задач 1.1"}]},{"name":"Доска 1"}];
-
-//let p1 = ["name","children"];
-//let p2 = ["Доска 1",[{"name": "Список задач 1.1"}]];
-
-//parse(modelBuilder(p1,p2).read());
 
 data = [{"id":1,"name":"Доска 1","hasChildren":true},{"id":2,"parent":1,"name":"Список задач 1.1","hasChildren":true},{"id":3,"parent":2,"name":"Задача 1.1.1"},{"id":4,"parent":2,"name":"Задача 1.1.2"},{"id":5,"parent":1,"name":"Список задач 1.2","hasChildren":true},{"id":6,"parent":5,"name":"Задача 1.2.1"},{"id":7,"parent":5,"name":"Задача 1.2.2"},{"id":8,"parent":1,"name":"Список задач 1.3"},{"id":9,"name":"Доска 2"}];
-
+DataList = {};
+/**
+ * 
+ * @param {Number} id 
+ * @param {Function} callback 
+ */
 function loadChildren(id, callback){
     if(callback === undefined || typeof(callback) !== 'function')
         throw new Error('callback not defined');
@@ -183,29 +109,65 @@ function loadChildren(id, callback){
     for (let i = 0; i < data.length; i++) {
         if(data[i].parent == id)
         {
-           res.push( new modelBuilder(data[i]))
+            let o = new modelBuilder();
+            o.update(data[i]);
+            res.push(o);
         }
     }
     if (res.length)
         setTimeout(callback, 0,res);
 }
+
+/**
+  * 
+  */
 class modelBuilder{
-    constructor(obj){
+    /** 
+     * @param {Array} keys Массив ключей.
+     * @param {Array} values Массив значений.
+     * @constructor 
+     */
+    constructor(keys,values){
         this.id = null;
         this.hasChildren = false;
-        this.name = 'defoult name';
+        this.name = '';
         this.parent = null;
-        this.update(obj);
-    }
+
+        if(keys && values)  //Если не передали массив ключей и значений то создаем пустой объект. 
+        {
+            if(keys.length !== values.length)    //Если размеры объектов не эквивалентны, то генерируем исключение.
+                throw new Error('Ключи и значения имеют разные размеры');
+            for (let i = 0; i < keys.length; i++) {  //Цикл обработки элементов массива.
+                res[keys[i]] = values[i];  //Создается новое поле с названием key[i] и с  значением values[i].
+            }
+        }
+    }    
+    /**
+     * Метод delete, помечает объект для удаления 
+     * (свойство removed принимает значение true).
+     *  @name modelBuilder#delete
+     */
     delete(){
         this.removed = true;  //Свойство removed принимает значение true.
     }
+    /**
+     * Метод update обновляет объект.
+     * @name modelBuilder#update
+     * @param {Object} updObj Предполагается что во входящем объекте updObj содержаться ключ/значения для обновления.
+     */
     update(updObj){
         for (const i in updObj) {  //Цикл по всем ключам в объекте.
             if(this[i] !== undefined) //Если поле существует, то изменить значение
                 this[i] = updObj[i];
         }
     }
+    /**
+     * Метод read возвращает выбранный (key) объект JSON таблицы.
+     * Если ключа не существует или значение в нём пусто – возвращает null.
+     * @name modelBuilder#read
+     * @param {String} key 
+     * @returns {Object} 
+     */
     read(key){
         if(this.removed)  //Проверка значения элемента. Если он печень как удаленный, то возвращаем undefined. 
             return; 
@@ -226,19 +188,25 @@ class modelBuilder{
         m[0] = tmp;
         return m;  //Возвращаем объект без методов.
     }
+    /**
+     * 
+     * @param {Function} callback 
+     */
     getChildren (callback){
-        setTimeout(callback,0,id,cb);
+        setTimeout(loadChildren,0,this.id,callback);
     }
+    /**
+     * @returns {String}
+     */
     render() {
-        let btn = '';
-        if(this.hasChildren)
-            btn = '<div class="list-open" onclick="OpenLine(this)" id="btn' + this.id + '"></div>';
-        return '<li id="list'+ this.id +'">' + btn + ' ' + this.name + '</li>';
+        let btn = '<div class="list-open" onclick="OpenLine(this)" id="btn' + this.id + '">' + this.name + '</div>';
+        return '<li id="list'+ this.id +'">' + btn + ' ' + '</li>';
     }
 }
+
 function cb(arr)
 {
-    let container = 'main-list'
+    let container = 'main-list';
     if(arr[0].parent)
     {
         container = 'list' +  arr[0].parent;
@@ -247,16 +215,16 @@ function cb(arr)
     let res = '';
     for (let i = 0; i < arr.length; i++) {
         res += arr[i].render();
+        DataList['btn'+arr[i].id] = arr[i];
     }
     li.innerHTML += '<ul>'+res+'</ul>';
 }
 
-function OpenLine(line){
-    console.log('open')
-    if(line.innerText === '0')
-        return ;
-    line.innerText = '0';
-    loadChildren(line.id.substring(3),cb);
+function OpenLine(line){    
+    if(!DataList[line.id].opened){
+        DataList[line.id].opened = true;
+        DataList[line.id].getChildren(cb);
+    }
 }
 
 loadChildren(null,cb);
